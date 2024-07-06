c4dfb5b3455084ea489f049f483870ad4c5312cd
Barton Meeks <Barton.Meeks@pobox.com>
2024-07-06 11:15:18 -0400

    Add support for iOS and macOS sizes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This little python script makes a boring image that is ment to be a used as a custom horizon
-in SkySafari, based on mesurements you make of your horizon obstructions. When using this
+in SkySafari, based on mesurements you make of your horizon obstructions. You can use this image 
+as is, or you can use this for a base layer to align photgraphs or panorama of your horizon.
+When using this
 custom horizon, you have a truer picture of the sky at your site, and you can more accurately
 tell if a particular object is actually visible at your site.
 
@@ -12,8 +14,8 @@ For example, if your horizon is
 at 20 degrees everywhere, except for a pesky mountain in the south that blocks the sky up to 45
 degrees (and is 40 degrees wide at the base, you would use a CSV file like this:
 
-`
+````
 160,20
 180,45
 200,20
-`
+```
--- makeSkySafariHorizon.py
+++ makeSkySafariHorizon.py
@@ -1,23 +1,33 @@
+#!/usr/bin/env python3
 import csv
 import sys
 from PIL import Image,ImageDraw
 
 # Constants
-BOT = 2047
+#    macOS size is 4096 x 2048
+# WIDTH = 4096
+# HEIGHT = 2048
+#    iOS size is 2048 x 1024
+WIDTH = 2048
+HEIGHT = 1024
 COLOR = (50, 200, 50, 200)
 
 horizon = []
+BOT = HEIGHT - 1
 
 def sort_key(row):
     return row[0]
 
 def AzToX(az):
-    return int(az*4096/360)
+    return int(az*WIDTH/360)
 
 # Note: Sky Safari supports horizons below 0 in its horizon panorama support, I believe.
 # However, this program expects the altitude to be between 0 and 90.
 def AltToY(alt):
-    return int(2048-(alt*1024/90+1024))
+    #    return int(2048-(alt*1024/90+1024))
+    HALF = HEIGHT/2
+    # return int(WIDTH-(alt*1024/90+1024))
+    return int(HEIGHT-(alt*HALF/90+HALF))
 
 def drawPoly(i):
     x1 = AzToX(horizon[i][0])
@@ -27,7 +37,9 @@ def drawPoly(i):
 
     draw.polygon([(x1,y1),(x2,y2),(x2,BOT),(x1,BOT)], fill=COLOR, outline=COLOR, width=1)
 
+###############################
 # Main program
+
 # Read the CSV file that describes the horizon
 
 with open('horizon.csv') as csvfile:
@@ -37,9 +49,11 @@ with open('horizon.csv') as csvfile:
         row[1] = float(row[1])
         horizon.append(row)
 
+# Sort the horizon list by azimuth
 horizon.sort(key = sort_key)
 
-img = Image.new(mode='RGBA', size=(4096,2048), color=(0,0,0,0))
+# Create a new 32 bit image with transparency
+img = Image.new(mode='RGBA', size=(WIDTH,HEIGHT), color=(0,0,0,0))
 draw = ImageDraw.Draw(img)
 
 k = 0
@@ -72,7 +86,7 @@ x2 = AzToX(Az2)
 y2 = AltToY(Alt2)
 y_wrap = AltToY(y_wrap)
 
-draw.polygon([(x1,y1), (4095,y_wrap), (4095,BOT), (x1,BOT)], fill=COLOR, outline=COLOR, width=1)
+draw.polygon([(x1,y1), ((WIDTH-1),y_wrap), ((WIDTH-1),BOT), (x1,BOT)], fill=COLOR, outline=COLOR, width=1)
 draw.polygon([(x2,y2), (   0,y_wrap), (   0,BOT), (x2,BOT)], fill=COLOR, outline=COLOR, width=1)
 
 img.save('image.png', 'PNG')
